1251e6cc672dc6ae3bb69218bb1025ab469f47f9
Heli Devine <heli.devine@gmail.com>
2022-02-03 18:00:13 +0000

    working with search logic

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,17 @@ import { useState } from 'react';
 import '../styles/Search.css';
 
 const Search = () => {
+	const [value, setValue] = useState();
 	return (
 		<>
-			<input className="search-input" type="text" />
+			<input
+				className="search-input"
+				type="text"
+				onChange={event => setValue(event.target.value)}
+			/>
+			<button className="search-button" type="submit">
+				search images
+			</button>
 		</>
 	);
 };
